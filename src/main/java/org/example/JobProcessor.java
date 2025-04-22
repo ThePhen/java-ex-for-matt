7d8779e6d47be218c7f4c5cec1a8b48550189522
Stephen Henderson <stephenhenderson8@gmail.com>
2025-04-22 16:35:26 -0500

    Using GOF's Chain of Responsibility pattern, rather than inheritance.

--- src/main/java/org/example/JobProcessor.java
+++ src/main/java/org/example/JobProcessor.java
@@ -1,24 +1,67 @@
 package org.example;
 
 import org.example.jobcontext.JobContext;
+import org.example.util.ProjectRootFactory;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class JobProcessor {
     private final JobContext ctx;
+    private File cachedProjectsRootDir;
 
     public JobProcessor(JobContext ctx) {
         this.ctx = ctx;
     }
 
+    private boolean areInputsGood() {
+        //run both checks — side-stepping short-circuit eval
+        final boolean a = isClientNameGood();
+        final boolean b = isProjectNameGood();
+        return a && b;
+    }
+
+    private File getJobProjectRootDir() {
+        final String path = getProjectsRootDir().getPath();
+        final String clientName = ctx.getClientName();
+        final String projectName = ctx.getProjectName();
+        final Path outputPath = Paths.get(path, clientName, projectName);
+        return outputPath.toFile();
+    }
+
+    private File getProjectsRootDir() {
+        if (!Objects.isNull(cachedProjectsRootDir)) return cachedProjectsRootDir;
+        cachedProjectsRootDir = ProjectRootFactory.projectsRootDir(ctx.getUserHomePath());
+        return cachedProjectsRootDir;
+    }
+
+    private boolean isClientNameGood() {
+        boolean isBad = isNullOrEmpty(ctx.getClientName());
+        if (isBad) ctx.logProgress("\nThe Client Name must not be blank.");
+        return !isBad;
+    }
+
+    private boolean isNullOrEmpty(String s) {
+        return (s == null || s.trim().isEmpty());
+    }
+
+    private boolean isProjectNameGood() {
+        boolean isBad = isNullOrEmpty(ctx.getProjectName());
+        if (isBad) ctx.logProgress("\nThe Project Name must not be blank.");
+        return !isBad;
+    }
+
     private int numInputFiles() {
-        File inputDir = new File(ctx.getJobProjectRootDir(), "Inputs");
+        File inputDir = new File(getJobProjectRootDir(), "Inputs");
         return inputDir.listFiles().length;
     }
 
     public void startProcessing() {
         // NOTE: This would be much more complicated. It's where the real work begins.
-        String msg = "Job Project Root (" + numInputFiles() + " Records found): " + ctx.getJobProjectRootDir() +
+        if (!areInputsGood()) throw new IllegalArgumentException("The input configuration is flawed. See above.");
+        String msg = "Job Project Root (" + numInputFiles() + " Records found): " + getJobProjectRootDir() +
                 "\nClient: " + ctx.getClientName() +
                 "\nProject: " + ctx.getProjectName() +
                 "\nStarting at (if any): " + ctx.getStartingSequenceNumber();
--- src/main/java/org/example/jobcontext/CmdArgsJobContext.java
+++ src/main/java/org/example/jobcontext/CmdArgsJobContext.java
@@ -4,75 +4,75 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
+import java.io.File;
 import java.util.Objects;
 
-@Parameters(separators = "=")
-public class CmdArgsJobContext extends BaseJobContext {
+public class CmdArgsJobContext implements JobContext {
 
-    @Parameter(names = {"-run-silent", "--run-silent"}, description = "When this flag is present, the job will " +
-            "run without any user intervention required. Used for job automation.")
-    private boolean runSilent = false;
+    private final JobContext next;
 
-    @Parameter(names = {"-c", "--client"}, description = "The Client name to process")
-    private String clientName = null;
+    private final ParmsFromCommandLine parms = new ParmsFromCommandLine();
 
-    @Parameter(names = {"-p", "--project"}, description = "The Client's Project to process")
-    private String projectName = null;
-
-    @Parameter(names = {"-s", "--start-num"}, description = "The sequence number of the first record to process")
-    private int startSeqNum = 1;
-
-    @Parameter(names = {"--user-home"}, description = "The path to the user's home. By default, the value " +
-            "the OS provides is used.")
-    private String userHomePath = determineUserHome();
-
-    @Parameter(names = {"-h", "--help"}, description = "Print help message", help = true)
-    private boolean help = false;
-
-    public CmdArgsJobContext(String[] args) {
+    public CmdArgsJobContext(String[] args, JobContext next) {
+        this.next = next;
         JCommander jCommander = JCommander.newBuilder()
-                .addObject(this)
+                .addObject(parms)
                 .build();
         jCommander.parse(args);
 
-        if (this.help) {
+        if (parms.help) {
             jCommander.usage();
             System.exit(0);
         }
     }
 
-    private static String determineUserHome() {
-        String envVar = System.getenv("SETTINGS_HOME");
-        if (Objects.nonNull(envVar)) return envVar;
-        return System.getProperty("user.home");
-    }
-
-    @Override
     public String getClientName() {
-        return clientName;
+        if (Objects.isNull(parms.clientName)) return next.getClientName();
+        return parms.clientName;
     }
 
-    @Override
     public String getProjectName() {
-        return projectName;
+        if (Objects.isNull(parms.projectName)) return next.getProjectName();
+        return parms.projectName;
     }
 
-    @Override
     public int getStartingSequenceNumber() {
-        return Math.min(10, Math.max(startSeqNum, 1));
+        return Math.min(10, Math.max(parms.startSeqNum, 1));
     }
 
-    String getUserHomePath() {
-        return userHomePath;
+    public String getUserHomePath() {
+        if (Objects.isNull(parms.userHomePath)) return next.getUserHomePath();
+        return parms.userHomePath;
     }
 
-    @Override
     public boolean isRunningSilent() {
-        return runSilent;
+        return parms.runSilent;
     }
 
-    @Override
     public void logProgress(String message) {
-        System.out.println(message);
+        next.logProgress(message);
+    }
+
+    @Parameters(separators = "=")
+    class ParmsFromCommandLine {
+        @Parameter(names = {"-run-silent", "--run-silent"}, description = "When this flag is present, the job will " +
+                "run without any user intervention required. Used for job automation.")
+        private boolean runSilent = false;
+
+        @Parameter(names = {"-c", "--client"}, description = "The Client name to process")
+        private String clientName = null;
+
+        @Parameter(names = {"-p", "--project"}, description = "The Client's Project to process")
+        private String projectName = null;
+
+        @Parameter(names = {"-s", "--start-num"}, description = "The sequence number of the first record to process")
+        private int startSeqNum = 1;
+
+        @Parameter(names = {"--user-home"}, description = "The path to the user's home. By default, the value " +
+                "the OS provides is used.")
+        private String userHomePath = null;
+
+        @Parameter(names = {"-h", "--help"}, description = "Print help message", help = true)
+        private boolean help = false;
     }
 }
